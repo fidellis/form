f38760ae43a62f47fd00275b676d4abcc4e354f6
Henrique Fidellis <fidellis@bb.com.br>
2023-06-21 21:19:07 -0300

    alt

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import Form from './Form';
+import DateInput from './inputs/DateInput';
+import NumberInput from './inputs/NumberInput';
+import PercentInput from './inputs/PercentInput';
+import Radio from './inputs/Radio';
+import Select from './inputs/Select';
+import SelectAsync from './inputs/SelectAsync';
+import Switch from './inputs/Switch';
+import TextArea from './inputs/TextArea';
+import TextInput from './inputs/TextInput';
+import TextInputCPF from './inputs/TextInputCPF';
+import TextInputCNPJ from './inputs/TextInputCNPJ';
+import Label from './inputs/Label';
+import Button from './Button';
+
+export { Form, DateInput, NumberInput, PercentInput, Radio, Select, SelectAsync, Switch, TextArea, TextInput, TextInputCPF, TextInputCNPJ, Label, Button };
+export default Form;
